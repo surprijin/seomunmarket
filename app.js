dfea529655767773798438b5b37b8ae8464eb7a3
surprijin <surprijin@naver.com>
2022-08-04 11:55:25 +0900

    commit

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var expressLayouts = require('express-ejs-layouts');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 // var path = require('path');
-const http = require('https');
+// const http = require('https');
 
-setInterval(function(){
-    http.get("https://seomunmarket.herokuapp.com/")
-},600000)
+// setInterval(function(){
+//     http.get("https://seomunmarket.herokuapp.com/")
+// },600000)
 
 
 const routers = require('./routes/route.js');
